a09fc3a6cf154004c3d0d595d9feaac9abf75479
Nishkarsh Vardhan <32043777+nishkarshv@users.noreply.github.com>
2020-06-05 20:16:28 +0530

    Update ayasdi_python_code.py
    Adding time taken code and changed the logic for db insert.

--- SymphonyAI/ayasdi_python_code.py
+++ SymphonyAI/ayasdi_python_code.py
@@ -2,21 +2,31 @@
 import db_operations as sql
 from mapping_random_data import Mapping_Random
 from create_csv import Create_Csv
-
+import multiprocessing as mp
+import time
 class Ayasdi:
     def main(self):
-        '''
-        Main function to run the complete task
-        '''
-        n = 1000000  
+        n = 1000000
         csvfilename = 'ayasdi_assignment.csv'
+        dbname = "ayasdi.db"
+        tablename = "ayasdi"
         start_date = 'January 1, 2014'
         end_date = 'December 31, 2014' 
         map_random = Mapping_Random()
         ccsv = Create_Csv()
+        start_time = time.time()
         csvmapping_dict = map_random.create_mapping_csv(n, start_date, end_date)
-        ccsv.create_csv(csvmapping_dict, csvfilename)
-        sql.csvToDatabase('ayasdi_assignment.csv','ayasdi.db','ayasdi_random', outputToFile = False)
+        print("--- mapping time is   --------------  %s seconds ---" % (time.time() - start_time))
+        pool = mp.Pool(mp.cpu_count())
+        csv_start_time = time.time()
+        pool.apply(ccsv.create_csv, args=[csvmapping_dict, csvfilename])
+        print("--- csv writing time is   --------------  %s seconds ---" % (time.time() - csv_start_time))
+        db_start_time = time.time()
+        pool.apply(sql.create_db_from_dataset, args=[dbname, tablename, csvmapping_dict])
+        print("--- db writing time is   --------------  %s seconds ---" % (time.time() - db_start_time))
+        
+        ################uncomment the below call to see data of table########################
+        #sql.get_data_from_table('ayasdi.db', 'ayasdi')
 if __name__ == "__main__":
     ay = Ayasdi()
     ay.main()
