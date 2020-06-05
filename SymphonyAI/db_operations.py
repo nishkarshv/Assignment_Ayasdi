65e48b9ebbc0a9babacd8b4c2a7885396ce82a9d
Nishkarsh Vardhan <32043777+nishkarshv@users.noreply.github.com>
2020-06-05 20:18:51 +0530

    Update db_operations.py
    Logic change in db insertion as to update the db in parallel with csv update.

--- SymphonyAI/db_operations.py
+++ SymphonyAI/db_operations.py
@@ -1,123 +1,147 @@
 import csv, sqlite3
 from sqlite3 import OperationalError 
 
-def _get_col_datatypes(csvfile):
-    '''
-    To create the column datatypes
-    '''
-    dr = csv.DictReader(csvfile, delimiter= "\t") 
-    
-    fieldTypes = {}
-    for entry in dr:
-        feildslLeft = [f for f in dr.fieldnames if f not in fieldTypes.keys()]        
-        if not feildslLeft: break 
-        for field in feildslLeft:
-            data = entry[field]
 
-            if len(data) == 0:
-                continue
-            
-            fieldTypes[field] = "TEXT"
-        
-
-    if len(feildslLeft) > 0:
-        raise Exception("Failed to find all the columns data types, please check again.")
-
-    return fieldTypes
-
-def create_connection(dbname):
-    '''
-    function to create connections
-    '''
-    try:
-        
-        connection =  sqlite3.connect(dbname)
-        return connection
-    except (ValueError, sqlite3.OperationalError):
-        import warnings
-        warnings.warn("Make sure the database file %s is installed and uncompressed." % dbname)
-        raise 
-    
-def execute_query(dbname, query):
-    '''
-    function to execute queries
-    '''
-    try:
-        
-        connection =  create_connection(dbname)
-        cur = connection.cursor()
-        return cur.execute(query)
-    except (ValueError, sqlite3.OperationalError):
-        import warnings
-        warnings.warn("Make sure the database file %s is installed and uncompressed." % dbname)
-        raise 
-
-def create_table_statement(dbname, tablename, columnlist):
-    '''
-    Generate create table statement:
-    '''
-    stmt = "create table if not exists \"" + tablename + "\" (%s)" % ",".join(columnlist)
-    return stmt
-        
-def insert_to_table_statement(tablename, sqldict):
-    '''
-    insert statement creation
-    '''
-    columns_string= '('+','.join(sqldict.keys())+')'  
-            
-    values_string = '('+','.join(map(str,sqldict.values()))+')'   
-    
-    sql = """INSERT INTO %s %s
-        VALUES %s"""%(tablename, columns_string,values_string)
-        
-    return sql
-
-def csvToDatabase(csvFile,dbname,tablename, outputToFile = False):
-    '''
-    csv to database function
-    '''
-  
-    with open(csvFile,mode='r', encoding="ISO-8859-1") as readcsv:
-        dt = _get_col_datatypes(readcsv)
-
-        readcsv.seek(0)
-
-        reader = csv.DictReader(readcsv, delimiter= "\t")
-        
-        # Keep the order of the columns name just as in the CSV
-        fields = reader.fieldnames
-        columnlist = []
-
-        # Set field and type
-        for f in fields:
-            columnlist.append("\"%s\" %s" % (f, dt[f]))
-        
-        # create table
-        create_table = create_table_statement(dbname, tablename, columnlist)
-        execute_query(dbname, create_table)
-        
-        
-        for row in reader:
-            # insert to table
-            sql = insert_to_table_statement(tablename, row)
-            try:
-                execute_query(dbname, sql)
-            except sqlite3.OperationalError:
-                pass
-    readcsv.close()
     
     
 def get_data_from_table(dbname, tablename):
     '''
     Select query from table
     '''
-    conn =  sqlite3.connect(dbname)
-    cur = conn.cursor()
+    
     try:
-        cur.execute("""SELECT * FROM ayasdi.ayasdi31""")
+        conn =  sqlite3.connect(dbname)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM {};".format(tablename))
+        results = cur.fetchall()
+        print(results)
     except OperationalError:
         return(False)        
         
 
+def create_db_from_dataset(dbname, tbl_name, dataset):
+    '''
+    create db, table and insert values to table
+    '''
+    conn =  sqlite3.connect(dbname)
+    cur = conn.cursor()                                 
+ 
+    columns = ','.join(['?'] * len(dataset.keys()))        
+    data = [tuple(t) for t in zip(*dataset.values())]
+    
+    cur.execute("drop table if exists %s" % tbl_name)
+    
+    col_str = '"' + '","'.join(dataset.keys()) + '"'
+    
+    cur.execute("create table %s (%s)" % (tbl_name, col_str))
+    
+    cur.executemany("insert into %s values(%s)" % (tbl_name, columns), data)
+    
+    conn.commit()
+    conn.close()
 
-#get_data_from_table('ayasdi.db', 'ayasdi31')
+############################BELOW is CODE to read CSV file and put data into database####################################
+# def _get_col_datatypes(csvfile):
+#     '''
+#     To create the column datatypes
+#     '''
+#     dr = csv.DictReader(csvfile, delimiter= "\t") 
+    
+#     fieldTypes = {}
+#     for entry in dr:
+#         feildslLeft = [f for f in dr.fieldnames if f not in fieldTypes.keys()]        
+#         if not feildslLeft: break 
+#         for field in feildslLeft:
+#             data = entry[field]
+
+#             if len(data) == 0:
+#                 continue
+            
+#             fieldTypes[field] = "TEXT"
+        
+
+#     if len(feildslLeft) > 0:
+#         raise Exception("Failed to find all the columns data types, please check again.")
+
+#     return fieldTypes
+
+# def create_connection(dbname):
+#     '''
+#     function to create connections
+#     '''
+#     try:
+        
+#         connection =  sqlite3.connect(dbname)
+#         return connection
+#     except (ValueError, sqlite3.OperationalError):
+#         import warnings
+#         warnings.warn("Make sure the database file %s is installed and uncompressed." % dbname)
+#         raise 
+    
+# def execute_query(dbname, query):
+#     '''
+#     function to execute queries
+#     '''
+#     try:
+        
+#         connection =  create_connection(dbname)
+#         cur = connection.cursor()
+#         return cur.execute(query)
+#     except (ValueError, sqlite3.OperationalError):
+#         import warnings
+#         warnings.warn("Make sure the database file %s is installed and uncompressed." % dbname)
+#         raise 
+
+# def create_table_statement(dbname, tablename, columnlist):
+#     '''
+#     Generate create table statement:
+#     '''
+#     stmt = "create table if not exists \"" + tablename + "\" (%s)" % ",".join(columnlist)
+#     return stmt
+        
+# def insert_to_table_statement(tablename, sqldict):
+#     '''
+#     insert statement creation
+#     '''
+#     columns_string= '('+','.join(sqldict.keys())+')'  
+            
+#     values_string = '('+','.join(map(str,sqldict.values()))+')'   
+    
+#     sql = """INSERT INTO %s %s
+#         VALUES %s"""%(tablename, columns_string,values_string)
+        
+#     return sql
+
+# def csvToDatabase(csvFile,dbname,tablename, outputToFile = False):
+#     '''
+#     csv to database function
+#     '''
+  
+#     with open(csvFile,mode='r', encoding="ISO-8859-1") as readcsv:
+#         dt = _get_col_datatypes(readcsv)
+
+#         readcsv.seek(0)
+
+#         reader = csv.DictReader(readcsv, delimiter= "\t")
+        
+#         # Keep the order of the columns name just as in the CSV
+#         fields = reader.fieldnames
+#         columnlist = []
+
+#         # Set field and type
+#         for f in fields:
+#             columnlist.append("\"%s\" %s" % (f, dt[f]))
+        
+#         # create table
+#         create_table = create_table_statement(dbname, tablename, columnlist)
+#         execute_query(dbname, create_table)
+        
+        
+#         for row in reader:
+#             # insert to table
+#             sql = insert_to_table_statement(tablename, row)
+#             try:
+#                 execute_query(dbname, sql)
+#             except sqlite3.OperationalError:
+#                 pass
+#     readcsv.close()
